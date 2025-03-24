2e1d46277b8048e29608b6e20e25297efcf805f7
boddedasairameshnaidu <rameshsai710@gmail.com>
2025-03-25 00:03:37 +0530

    First commit

--- expense_manager/settings.py
+++ expense_manager/settings.py
@@ -116,9 +116,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+# Define the folder where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Make sure STATIC_URL is also defined
+STATIC_URL = '/static/'
+
+# Add this if not already present
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
